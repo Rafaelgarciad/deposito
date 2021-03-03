5d5c8627f64993a77d8a483cbe1f877934f1b26f
garri <garri@10.0.2.15>
2021-03-03 13:50:54 +0100

    Realizado Test estaLleno

---
+++ DepositoCombustible/Test/deposito/DepositoCombustibleTest.java
@@ -0,0 +1,17 @@
+package deposito;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DepositoCombustibleTest {
+
+	@Test
+	public void testEstaLleno() {
+		DepositoCombustible tank = new DepositoCombustible(40.0,0.0);
+		tank.fill(40.0);
+		assertTrue(tank.estaLleno());
+		assertFalse(tank.estaVacio());
+	}
+
+}
